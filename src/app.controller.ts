c1fe0ae89adf3c80f2201623eb4a4fc0985fb063
zhaosheng2000 <zs17337825170@163.com>
2023-12-11 19:25:46 +0800

    sse

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Sse } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+const { exec } = require("child_process");
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Sse('stream')
+  stream() {
+    const childProcess = exec('tail -f ./log');
+
+    return new Observable((observer) => {
+
+      childProcess.stdout.on('data', (msg) => {
+        observer.next({ data: { msg: msg.toString() }});
+      })
+      
+    });
+  }
+}
